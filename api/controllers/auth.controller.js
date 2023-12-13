8d95b096271dc82959466ac7761c3d487b44fb8b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-13 22:27:04 +0530

    fix error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,7 +58,7 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
-        username: req.body.username.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
+        username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -29,6 +29,7 @@ export default function OAuth() {
         }),
       });
       const data = await res.json();
+       console.log(data); 
       dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
