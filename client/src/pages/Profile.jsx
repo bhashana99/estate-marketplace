d554e52f382479d4359f7f174bdd2929cf88fd27
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-20 12:19:29 +0530

    complete update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -272,7 +272,10 @@ export default function Profile() {
 
           <div className="flex flex-col items-center">
             <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">delete</button>
-            <button className="text-green-700 uppercase">edit</button>
+            <Link to={`/update-listing/${listing._id}`}>
+              <button className="text-green-700 uppercase">edit</button>
+            </Link>
+            
           </div>
         </div>
       ))}
